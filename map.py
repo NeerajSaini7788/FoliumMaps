383067e08dc7a716433b7734d5e6778aa5f6d372
Neeraj Saini <asaini7788@gmail.com>
2020-10-11 16:29:16 -0400

    Add Markers Feature Group

--- map.py
+++ map.py
@@ -1,4 +1,15 @@
 import folium
+import pandas as pd
+
+#Reading the data file
+data = pd.read_csv("Volcanoes_USA.txt")
+
+#converting pd column into a list
+lat = list(data["LAT"])
+lon = list(data["LON"])
+name = list(data["NAME"])
+
+
 map = folium.Map(location=(45.540470, -73.668482) , zoom_start = 5, width = "75%" , height = "75%" , min_zoom = 2 )
 map.add_child(folium.TileLayer('Stamen Terrain'))
 map.add_child(folium.TileLayer('Stamen Toner'))
@@ -7,6 +18,14 @@ map.add_child(folium.TileLayer('cartodbpositron'))
 
 
 
+#mapping the Markers
+Markers_fg = folium.FeatureGroup(name = "My Map")
+for lat, lon,name in zip(lat,lon,name):
+    Markers_fg.add_child(folium.Marker(location = (lat,lon) , popup = name , icon = folium.Icon(color="green")))
+
+
+#Adding the feature group to the map
+map.add_child(Markers_fg)
 
 
 map.add_child(folium.LayerControl())
